f798cf90d9f06dedb749fbdd7ef2428bcaf6e153
grosenbe <geoff.rosenberg@gmail.com>
2018-04-28 13:22:29 -0500

    Merged the .emacs changes in from LinuxLaptop.

--- .emacs
+++ .emacs
@@ -7,7 +7,6 @@
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(custom-enabled-themes (quote (manoj-dark)))
  '(inhibit-startup-screen t)
- '(send-mail-function (quote smtpmail-send-it))
  '(show-paren-mode t))
 
 (custom-set-faces
@@ -27,8 +26,8 @@
 
 (delete-selection-mode 1)
 
+;; convenience stuff
 (global-set-key (kbd "<C-tab>") 'other-window)
-
 (require 'ido)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
@@ -41,3 +40,15 @@
 
 ;; always default to many windows in GDB
 (setq gdb-many-windows t)
+
+;; set org mode up for literal programming
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((C . t)
+   (fortran . t)))
+
+;; melpa packages
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
--- .gnus.el
+++ .gnus.el
@@ -4,19 +4,50 @@
 (setq gnus-select-method
       '(nnimap "gmail"
 	       (nnimap-address "imap.gmail.com")
-	       (nnimap-server-port "imaps")
+	       (nnimap-server-port 993)
 	       (nnimap-stream ssl)))
 
+;; Gmail system labels have the prefix [Gmail], which matches
+;; the default value of gnus-ignored-newsgroups. That's why we
+;; redefine it.
 (setq smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587
       gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+
+(setq gnus-agent nil)
+
+;; We don't want local, unencrypted copies of emails we write.
+(setq gnus-message-archive-group nil)
+
+;; We want to be able to read the emails we wrote.
+(setq mml2015-encrypt-to-self t)
+
+;; Attempt to encrypt all the mails we'll be sending.
+;; can just delete the setting when typing the message if desired
+(add-hook 'message-setup-hook 'mml-secure-message-encrypt)
 
 (setq gnus-thread-sort-functions
       '((not gnus-thread-sort-by-number)
 	(not gnus-thread-sort-by-date)))
 
 (setq gnus-summary-line-format
-      "%U %D %s\n")
+      "%B %U %D %s %F %R\n")
 
-;; turn threading off on gnus
-;; (setq gnus-show-threads nil)
+;; Add two key bindings for your Gmail experience.
+(add-hook 'gnus-summary-mode-hook 'my-gnus-summary-keys)
+
+(defun my-gnus-summary-keys ()
+  (local-set-key "y" 'gmail-archive)
+  (local-set-key "$" 'gmail-report-spam))
+
+(defun gmail-archive ()
+  "Archive the current or marked mails.
+This moves them into the All Mail folder."
+  (interactive)
+  (gnus-summary-move-article nil "nnimap+imap.gmail.com:[Gmail]/All Mail"))
+
+(defun gmail-report-spam ()
+  "Report the current or marked mails as spam.
+This moves them into the Spam folder."
+  (interactive)
+  (gnus-summary-move-article nil "nnimap+imap.gmail.com:[Gmail]/Spam"))
