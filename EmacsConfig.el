c9bf5680a3159e576a01ced0100dca663177bf02
Geoff Rosenberg <geoff.rosenberg@gmail.com>
2021-08-14 15:40:34 -0700

    Silence native compilation warnings.

--- EmacsConfig.el
+++ EmacsConfig.el
@@ -20,6 +20,7 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(setq native-comp-async-report-warnings-errors '"silent")
 
 (defun server-shutdown ()
   "Save buffers, quit, and kill server"
